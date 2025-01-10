414720deb8596e60c1f11bb53c3a54f7fb10834e
fake-dp <fakecoko92@gmail.com>
2025-01-10 15:52:56 +0900

    앱제이슨 분기처리

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import appConfig from './app.json';
+
+const appName = Platform.OS === 'ios' ? appConfig.ios?.name ?? appConfig.name : appConfig.name;
 
 AppRegistry.registerComponent(appName, () => App);
